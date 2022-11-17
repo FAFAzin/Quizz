6fa6dc1882c15f3421fa490b191b6ff5528ffbbd
JosafaSilveira <josafajosina@gmail.com>
2022-11-17 17:15:47 -0300

    estagios

--- quizz/src/components/Welcome.jsx
+++ quizz/src/components/Welcome.jsx
@@ -8,13 +8,12 @@ import './Welcome.css';
 
 const Welcome = () => {
 
-    const quizState = useContext(QuizContext);
-    console.log(quizState)
+    const [quizState, dispatch] = useContext(QuizContext);
     return (
         <div id='Welcome'>
             <h2>Seja bem-vindo</h2>
             <p>Clique no botão abaixo para começar</p>
-            <button>Iniciar</button>
+            <button onClick={() => dispatch({type:'CHANGE_STATE'})}>Iniciar</button>
             <img src={Quiz} alt="Inicio do Quiz" />
         </div>
     )
